3646e3b7a1241b96252c87ca3b9ffcf0c320231e
KevinW0 <kevin.wang.02@outlook.com>
2021-01-01 08:18:10 -0500

    Functions complete, about to merge into one component

--- components/CreateAndJoin.js
+++ components/CreateAndJoin.js
@@ -5,6 +5,52 @@ import { TouchableHighlight, Text, View, SafeAreaView, StyleSheet} from 'react-n
 import { Button } from 'react-native-paper';
 
 
+{/* server Params */}
+
+const SERVER_ADDRESS = 'localhost:5000'
+
+
+{/*client states*/}
+
+
+
+
+
+async function requestCodeAndSetState (){
+    const code = await fetch(SERVER_ADDRESS.concat('/getCode'))
+    .then(resp => (setCode(code), console.log("should be done"), setErrorBool(0), showCodeResponse(1)))
+    .catch (error => (console.log ("Server couldn't be reached."), setErrorBool(1), setCode(""), showCodeResponse(1)));
+}
+
+
+const DisplayCodeOrError = ({code}) =>{
+{/*  errorBool:1 means error has occured. zero means it hasn't. */}
+const [errorBool, setErrorBool] = React.useState(null);
+
+
+if (errorBool === 1){
+    return(
+        <Text> Sorry, an error has occured contacting the server. </Text>
+    )
+}
+else if (errorBool === 0){
+    return (
+        <Text> Your Pomodoro code is (share with friends!): {code}</Text>
+    )
+
+}
+
+else {
+    return (<Text> This case should've never been hit. If you see this message, please contact the developer </Text>)
+
+
+}
+
+}
+
+
+
+
 const JoinLobbyInput = () => {
     [code, setCode] = React.useState("");
 
@@ -21,7 +67,7 @@ const LobbyButtons = () => {
     return (
         <View style={{flexDirection: "row", paddingVertical:1}}>
 
-        <Button style = {styles.flexeven} mode="outlined" onPress= {() => console.log ("to add code retrieve function")}>
+        <Button style = {styles.flexeven} mode="outlined" onPress= {() => requestCodeAndSetState()}>
         Generate Lobby
         </Button>
         <Button style = {styles.flexeven} mode="outlined"> Join Lobby </Button>
@@ -37,17 +83,23 @@ const LobbyButtons = () => {
 }
 
 
-const JoinAndCreateComp = () => 
-(
+const JoinAndCreateComp = () =>
+{[showCodeResponse, setShow] = React.useState(0);
+
+
+return (
     <View style= {{paddingVertical:1 }}>
     <JoinLobbyInput/>
+    {showCodeResponse===1? (
+        <DisplayCodeOrError code={code}> </DisplayCodeOrError>):
+        null}
     <LobbyButtons/>
     </View>
 
 
 
 
-)
+)}
 
 const styles = StyleSheet.create({
     flexeven:{flex: 1}
